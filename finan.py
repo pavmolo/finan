e67ba94235ec4c8ddbe9cce44f20f404fe546e65
pavmolo <74321783+pavmolo@users.noreply.github.com>
2023-02-26 10:25:50 +0300

    Update finan.py

--- finan.py
+++ finan.py
@@ -6,11 +6,11 @@ import plotly.express as px
 
 # Функция приложения
 def show_predict_page():
-    #image = Image.open('https://www.kaizen.com/images/kaizen_logo.png')
-    #st.image(image, caption='Kaizen Institute')
     st.markdown('''<a href="http://kaizen-consult.ru/"><img src='https://www.kaizen.com/images/kaizen_logo.png' style="width: 50%; margin-left: 25%; margin-right: 25%; text-align: center;"></a><p>''', unsafe_allow_html=True)
-    st.title("Определи свой потенциал")
-    
+    val = st.text_input("Единица измерения", value="Тыс. руб., Млн. руб. или что-либо другое")
+    st.title("Анализ ликвидности")
+    stroka_1250 = st.number_input(f"Денежные средства и денежные эквиваленты (строка 1250), {val} на конец пероида:", value=0)
+    stroka_1240 = st.number_input(f"Финансовые вложения (за исключением денежных эквивалентов) (строка 1240), {val} на конец пероида:", value=0)
 
 # Вызываем приложение
 
