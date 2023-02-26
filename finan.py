cb8e608476ab223e88a2b5b99a2baa9797e2ae3c
pavmolo <74321783+pavmolo@users.noreply.github.com>
2023-02-26 10:10:22 +0300

    Update finan.py

--- finan.py
+++ finan.py
@@ -1,1 +1,17 @@
+import streamlit as st
+import numpy as np
+import pandas as pd
+import plotly.graph_objects as go
+import plotly.express as px
 
+# Функция приложения
+def show_predict_page():
+    #image = Image.open('https://www.kaizen.com/images/kaizen_logo.png')
+    #st.image(image, caption='Kaizen Institute')
+    st.markdown('''<a href="http://kaizen-consult.ru/"><img src='https://www.kaizen.com/images/kaizen_logo.png' style="width: 50%; margin-left: 25%; margin-right: 25%; text-align: center;"></a><p>''', unsafe_allow_html=True)
+    st.title("Определи свой потенциал")
+    
+
+# Вызываем приложение
+
+show_predict_page()
