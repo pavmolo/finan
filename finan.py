cfcfe4b1e62847084e6d9e948cfd7f1312311023
pavmolo <74321783+pavmolo@users.noreply.github.com>
2023-02-26 10:46:25 +0300

    Update finan.py

--- finan.py
+++ finan.py
@@ -27,6 +27,9 @@ def show_predict_page():
     stroka_1530 = st.number_input(f"Доходы будущих периодов (строка 1530), {val} на конец пероида:", value=0)
     stroka_1540 = st.number_input(f"Оценочные обязательства (строка 1540), {val} на конец пероида:", value=0)
     
+    st.header('Группы ликвидности')
+    st.subheader(f'Самые ликвидные (А1) = {stroka_1250 + stroka_1240}')
+    
 # Вызываем приложение
 
 show_predict_page()
